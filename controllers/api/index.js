248667f041c85aea5d962ec1c3da20cbf058df0e
garretthilberling <ghilberling@knights.ucf.edu>
2022-04-01 13:26:35 -0400

    finished Reply model and API routes and index associations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const petRoutes = require('./pet-routes');
 const commentRoutes = require('./comment-routes');
+const replyRoutes = require('./reply-routes');
 
 router.use('/users', userRoutes);
 router.use('/pets', petRoutes);
 router.use('/comments', commentRoutes);
+router.use('/replies', replyRoutes);
 
 module.exports = router;
