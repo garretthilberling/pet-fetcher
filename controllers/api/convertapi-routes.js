d2f8e812d89cb10e6af605df6d07dd74bbb77e99
garretthilberling <ghilberling@knights.ucf.edu>
2022-04-03 00:01:20 -0400

    API works!

--- controllers/api/convertapi-routes.js
+++ controllers/api/convertapi-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
+const { Pet } = require('../../models');
 var convertapi = require('convertapi')(process.env.API_SECRET);
 require('dotenv').config();
 
@@ -16,6 +17,24 @@ router.post('/', (req, res) => {
   })
   .then(function(file) {
     console.log("File saved: " + file);
+    var imgConvert = res.send(file);
+    Pet.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['pic_filename']
+    })
+    .then(convertImg => {
+        if (!convertImg) {
+            res.status(404).json({ message: 'No pet found with that id' });
+            return;
+        }
+        res.json(convertImg);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
   })
   .catch(function(e) {
     console.error(e.toString());
