b50901f581d4b40570b002e3839fd0a89454e84e
anav <anav2096@gmail.com>
2022-04-04 23:29:31 -0400

    Seeds update

--- seeds/pet-seeds.js
+++ seeds/pet-seeds.js
@@ -2,33 +2,19 @@ const { Pet } = require('../models');
 
 const petdata = [
     {
-<<<<<<< HEAD
+
       bio: 'sled dog',
       pet_name: 'White Fang',
       species: 'dog',
       breed: 'husky',
       size: 'large',
-<<<<<<< HEAD
-      age: 2,
-      owner_id: 3,
-      pic_filename:'picture'
-=======
       age: 'adult',
       pic_filename: "https://www.petpaw.com.au/wp-content/uploads/2014/04/Boxer-Dog-3.jpg",
       user_id: 1
->>>>>>> 8297c9a79f784ab91eb12c4a5879d19ec366bde3
-
-    }
-=======
-      bio: 'Male fawn boxer, black mask, white markings',
-      pet_name: 'ButterFinger',
-      species: 'dog',
-      breed: 'boxer',
-      size: 'small',
-      age: 1,
-      owner_id: 1
 
     },
+
+  
     {
       bio: 'Male sealed brindle/ black boxer, white markings',
       pet_name: 'Milkyway',
@@ -69,9 +55,17 @@ const petdata = [
       owner_id: 1
 
     },
-    
+    {
+      bio: 'Female sealed brindle/ black boxer, white markings',
+      pet_name: 'Snickers',
+      species: 'dog',
+      breed: 'boxer',
+      size: 'small',
+      age: 1,
+      owner_id: 1
 
->>>>>>> main
+    },
+
 ];
 
 const seedPets = () => Pet.bulkCreate(petdata);
