a30e1410ebc01ad190db1ec209e44375b49d7625
anav <anav2096@gmail.com>
2022-04-05 16:19:39 -0400

    edit/delete comment

---
+++ public/javascript/edit-comment.js
@@ -0,0 +1,27 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const comment_text = document.getElementById('comment_text').value.trim();
+ 
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/comments/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        comment_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.getElementById('edit-comment-form').addEventListener('submit', editFormHandler);
+  
